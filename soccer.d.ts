b1b4d9983abf4998d9fefd2689b1a1325e01463c
Mattias Hising <hising@gmail.com>
2024-06-15 22:14:51 +0200

    Add soccer related types in soccer.d.ts
    This commit introduces various types related to the soccer domain in the soccer.d.ts file. These types include, but are not limited to, Player, Team, Match, Group, and Arena. With these new types, the project will have a more strong and rigid structure regarding soccer-related data.

---
+++ soccer.d.ts
@@ -0,0 +1,61 @@
+type Player = {
+    number: number
+    name: string
+    position: string
+    caps: number
+    goals: number
+    club: string
+}
+
+type Team = {
+    name: string;
+    code: string;
+}
+
+type Group = {
+    name: string;
+    teams: Team[];
+}
+
+type Round = {
+    name: string;
+    matches: Match[]
+}
+
+type Score = {
+    ft: string
+    ht: string
+}
+
+type Goals = {
+    name: string
+    minute: number
+    offset?: number
+    penalty?: boolean
+    owngoal?: boolean
+}
+
+type Match = {
+    num: number
+    date: string
+    time: string
+    home: Team
+    away: Team
+    score: Score
+    goalsHome: Goals[];
+    goalsAway: Goals[];
+    group: Group
+}
+
+type ArenaLocation = {
+    lat: number
+    lng: number
+}
+
+type Arena = {
+    name: string
+    city: string
+    country: string
+    capacity: number
+    location: ArenaLocation
+}
